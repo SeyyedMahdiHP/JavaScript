3cce0558d74f21c9ad0e1ef937cb4699dab456dc
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-15 11:34:31 +0330

    add JSON.stringify(object, null, 4)

--- Abstraction_and_modeling_support.js
+++ Abstraction_and_modeling_support.js
@@ -1,12 +1,16 @@
 /*Association*/
 function Person(name, surName) {
-  this.name = name;
-  this.surName = surName;
-  this.parent = null;
+    this.name = name;
+    this.surName = surName;
+    this.parent = null;
 }
 var seyyedMahdi = new Person("SeyyedMahdi", "HassanPour");
 var fatemehSadat = new Person("FatemehSadat", "HassanPour");
 fatemehSadat.parent = seyyedMahdi;
+
+var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+if (isFirefox) {alert(fatemehSadat.parent.toSource()); }// just in firefox
+else {alert(JSON.stringify(fatemehSadat, null, 4));} // other browsers
 
 /*Aggregation*/
 var company = {
@@ -17,6 +21,7 @@ var seyyedMahdi = new Person("SeyyedMahdi", "Hassanpour");
 var hamed = new Person("Hamed" ,"Naqiniarami");
 company.employees.push(seyyedMahdi);
 company.employees.push(hamed);
+alert(JSON.stringify(company, null, 4));
 
 /*Composition*/
 var person = {
@@ -28,3 +33,5 @@ var person = {
     country: "Iran"
     }
 };
+alert(JSON.stringify(person, null, 4));
+
