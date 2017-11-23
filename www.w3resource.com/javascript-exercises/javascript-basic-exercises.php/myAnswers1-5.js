17f70296cb5ee3fba15c890eaef9394e2e81f98f
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-23 15:06:06 +0330

    exercise3 ver1

--- www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers1-5.js
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers1-5.js
@@ -41,11 +41,6 @@ console.log(today.toString());
 console.log("Today is: "+ daylist[theDay]);
 console.log("Current Time is: "+theHours+" : "+minutes+" : "+seconds);
 
-
-
-
-
-
 /*
 2. Write a JavaScript program to print the contents of the current window.  Go to the editor
 Click me to see the solution
@@ -54,6 +49,20 @@ Click me to see the solution
 window.print();
 
 /*
-
+Write a JavaScript program to get the current date.  Go to the editor
+Expected Output : 
+mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy 
 */
+var today = new Date();
+var theDay = today.getDate();
+theDay = (theDay<10) ? '0'+theDay : theDay;
+var theMonth = today.getMonth()+1;
+theMonth = (theMonth<10)? '0'+theMonth : theMonth;
+var theYear = today.getFullYear();
+console.log(theDay+"-"+theMonth+"-"+theYear);
+
+/*
+4. Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.  
+*/
+
 
