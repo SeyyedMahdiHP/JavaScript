b53281d4db5368d16e7cb45be077c18d8dd64621
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-23 11:00:36 +0330

    ex1-v3

--- www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers.js
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers.js
@@ -5,14 +5,18 @@
 Sample Output : Today is : Friday. 
 Current time is : 4 PM : 50 : 22
 */
-var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var rightNow = new Date();
-var dataString = rightNow.toString();
-var theDay = rightNow.getDay();
-var theHours = function (hours){
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var today = new Date();
+var theDay = today.getDay();
+var theHours = today.getHours();
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
+
+var theHours = function (hours=0){
   var amPm = (hours>=12) ? " PM" : " AM";
   hours = (hours >=12) ? hours-12: hours;
-  return (hours + amPm).toUpperCase();}(rightNow.getHours());
-console.log(dataString);
-console.log("Today is: "+ dayNames[theDay]);
-console.log("Current Time is: "+theHours()+" : "+rightNow.getMinutes()+" : "+rightNow.getSeconds());
+  return (hours + amPm).toUpperCase();}(theHours);
+
+console.log(today.toString());
+console.log("Today is: "+ daylist[theDay]);
+console.log("Current Time is: "+theHours+" : "+minutes+" : "+seconds);
