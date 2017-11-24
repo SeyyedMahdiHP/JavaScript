a02d4ba6a8f07de6525c482276d00f36b53dbf10
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-24 13:14:02 +0330

    ex1-ver3

--- www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
@@ -12,17 +12,14 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 */
 //sol1:It simply checks if the day before the 1st March of the specified year is the 29th and if so then it must be a leap year. 
 //As long as the correct leap year rules are built into JavaScript you don't even need to know those rules in order to use it.
-var y = prompt("enter a year",2010);
-var leapdate = new Date(y, 2, 0);
-if (leapdate.getDate() == 29) alert(y+' is a leap year');
-else alert(y+' is not a leap year');
-//sol2:
+//https://codepen.io/SeyyedMahdiHP/pen/LOrLXO
+var year = prompt("enter a year",2010);
 var isLeap = function(year){
-  return new Date(year,1,29).getDate() ==29;
+  return new Date(year,1,29).getDate() ==29; //new Date(year,2,0).getDate() ==29
 }
-isLeap(2010);   //false
-isLeap(2014);   //false
-isLeap(2016);   //true
+console.log(isLeap(2010));   //false
+console.log(isLeap(2014));   //false
+console.log(isLeap(2016));   //true
 //sol3:
 year = window.prompt("Input a Year : ");
 x = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
