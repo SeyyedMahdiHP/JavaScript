9fc60ed89a79234e2058c72189fb24cc349a6f99
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-24 12:50:43 +0330

    ex1-ver1

--- www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
@@ -4,3 +4,20 @@
 /*
 6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
 */
+//sol1:
+var y = prompt("enter a year",2010);
+var leapdate = new Date(y, 2, 0);
+if (leapdate.date == 29) alert(y+' is a leap year');
+else alert(y+' is not a leap year');
+//sol2:
+var isLeap = function(year){
+  return new Date(year,1,29).getDate() ==29;
+}
+isLeap(2010);   //false
+isLeap(2014);   //false
+isLeap(2016);   //true
+//sol3:
+year = window.prompt("Input a Year : ");
+x = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+alert(x);
+
