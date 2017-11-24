b8e77a50b9131b814dff7414ad14649b003ba550
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-24 13:15:50 +0330

    ex1-ver4 add explanation

--- www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
@@ -3,7 +3,17 @@
 //use https://codepen.io/ to solve the problem
 /*
 6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
-Date Syntax:
+Explanation:
+
+A leap year is a year containing one additional day added to keep the calendar year synchronized with 
+the astronomical or seasonal year. Because seasons and astronomical events do not repeat in a whole number of days, 
+calendars that have the same number of days in each year drift over time with respect to the event that the year is supposed to track.
+By inserting an additional day or month into the year, the drift can be corrected. A year that is not a leap year is called a common year.
+Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100,
+but these centurial years are leap years if they are exactly divisible by 400. 
+For example, the years 1700, 1800, and 1900 are not leap years, but the year 2000 is.
+
+  Date Syntax:
 new Date();
 new Date(value);
 new Date(dateString);
