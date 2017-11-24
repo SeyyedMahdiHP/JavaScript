720b6ea765d8d3ec316193cb273ee01cff8e7c26
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-24 12:14:57 +0330

    ex6

---
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
@@ -0,0 +1,6 @@
+// in the name of Allah
+// https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+//use https://codepen.io/ to solve the problem
+/*
+6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
+*/
