f8216a22b60129cdb2909ac4b15c80a86ed78c8a
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-24 13:05:38 +0330

    ex1-ver2

--- www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
@@ -3,11 +3,18 @@
 //use https://codepen.io/ to solve the problem
 /*
 6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
+Date Syntax:
+new Date();
+new Date(value);
+new Date(dateString);
+new Date(year, month, day, hours, minutes, seconds, milliseconds);
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 */
-//sol1:
+//sol1:It simply checks if the day before the 1st March of the specified year is the 29th and if so then it must be a leap year. 
+//As long as the correct leap year rules are built into JavaScript you don't even need to know those rules in order to use it.
 var y = prompt("enter a year",2010);
 var leapdate = new Date(y, 2, 0);
-if (leapdate.date == 29) alert(y+' is a leap year');
+if (leapdate.getDate() == 29) alert(y+' is a leap year');
 else alert(y+' is not a leap year');
 //sol2:
 var isLeap = function(year){
