cc4efef881ae12952ea5898c600642a222713b42
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-11-30 11:06:58 +0330

    q8 ver2 mozila math

--- www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers6-10.js
@@ -83,6 +83,7 @@ alert(x);
 8. Write a JavaScript program where the program takes a random integer between 1 to 10,
 the user is then prompted to input a guess number. If the user input matches with guess number,
 the program will display a message "Good Work" otherwise display a message "Not matched". 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
 //https://thimbleprojects.org/seyyedmahdi/368191/
 <!DOCTYPE html>
