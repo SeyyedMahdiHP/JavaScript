43740c4d6462a5fe02ac38a6165bc3a500daf9b7
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-12-01 18:12:33 +0330

    FirstClass function, High order function

---
+++ www.w3resource.com/javascript-exercises/MiscQuestions.js
@@ -0,0 +1,9 @@
+
+1.What are the benefits of FirstClass Functions?
+//var print = console.log();
+// var shopping = function(args,...){};
+2.What are the benefits of High-Order Functions?
+// var shopping= function(item, function(){});
+var shopping = function(){
+return function();
+};
