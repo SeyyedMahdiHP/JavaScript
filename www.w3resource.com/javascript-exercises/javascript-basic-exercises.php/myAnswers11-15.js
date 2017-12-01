a4b3c0217a7473c438c6d7bc23faf2f1f563a6b7
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-12-01 09:21:47 +0330

    Q11 ver1

---
+++ www.w3resource.com/javascript-exercises/javascript-basic-exercises.php/myAnswers11-15.js
@@ -0,0 +1,5 @@
+// in the name of Allah
+// https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+/*
+11. Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.  
+*/
