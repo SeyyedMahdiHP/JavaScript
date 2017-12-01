0bad05b13d03df413e970b145eeff42e686dee6c
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-12-01 18:41:14 +0330

    add doNothing function

--- www.w3resource.com/javascript-exercises/useFullCodeSnippets/useFirstClassFunction_for_Debugging.js
+++ www.w3resource.com/javascript-exercises/useFullCodeSnippets/useFirstClassFunction_for_Debugging.js
@@ -4,16 +4,17 @@ if you wnat to print debug messages use below style and set DEBUG_MODE_ENABLED t
 then every place that has a "debug('...')" will show the message
 */
 const DEBUG_MODE_ENABLED = true;
+var debug;
 function printDebugMessage(message){
   console.log("DEBUG: "+ message);
 }
-
+function doNothing(message){
+  return;
+}
 if(DEBUG_MODE_ENABLED){
   debug = printDebugMessage;
 }
 else{
   debug = doNothing;
 }
-
-
 debug("Some Bug Message");
