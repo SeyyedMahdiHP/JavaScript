c92470cdb27659fb9ec28793dac5640be1f5e388
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-12-01 18:48:38 +0330

    add simpler debug

--- www.w3resource.com/javascript-exercises/useFullCodeSnippets/useFirstClassFunction_for_Debugging.js
+++ www.w3resource.com/javascript-exercises/useFullCodeSnippets/useFirstClassFunction_for_Debugging.js
@@ -3,6 +3,7 @@
 if you wnat to print debug messages use below style and set DEBUG_MODE_ENABLED to "true"
 then every place that has a "debug('...')" will show the message
 */
+//in the first one you can add multiple functionality in each if else if and ... with a new function to "debug"
 const DEBUG_MODE_ENABLED = true;
 var debug;
 function printDebugMessage(message){
@@ -18,3 +19,13 @@ else{
   debug = doNothing;
 }
 debug("Some Bug Message");
+
+
+
+// or :
+const DEBUG_MODE_ENABLED = true;
+var debug = function(message){
+  if(DEBUG_MODE_ENABLED){ console.log("DEBUG: "+ message);}
+};
+debug("Some Bug Message");
+
