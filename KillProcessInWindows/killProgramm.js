d911de0271f880376bb6570b35be0f6184c6e0e4
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-12-06 19:01:51 +0330

    add Async execution
    this version has read from console Synchron method and sync/async method for cmd Execution

--- KillProcessInWindows/killProgramm.js
+++ KillProcessInWindows/killProgramm.js
@@ -3,23 +3,31 @@
 // smahdi1991@gmail.com
 // webhoma.ir
 // this is a console app, that by using it, you can set a time in Minutes to kill a programm after the time has been passed.
-const execSync = require('child_process').execSync;
-var readConsole = require('readline-sync');
+const execSync = require('child_process').execSync; //synchron cmd exec
+const execASync = require('node-cmd'); 				//Asynchron cmd exec
+
+var readConsole = require('readline-sync'); // readConsole synchron
 var print = console.log;
-var run = function(commands){
-	var code = execSync(commands);
-	print(code.toString());
+
+var run = function(commands, method){
+	if(method === "sync"){
+	print(execSync(commands).toString());
+	}
+	else{
+		execASync.get(commands, (err, data, stderr)=>{print(data);});
+	}
 };
 print("Programm Started!");
-run('tasklist');
+run('tasklist', "sync");
 var pName =  function(){
 	var pname = readConsole.question("Enter  the Name of Programm you Want to Kill:");
 	return (pname.split('.').pop() === "exe")? pname: pname +".exe"; // check the extension
 }();
 var time = Number(readConsole.question("Enter time in 'Minutes' to Wait for Kill the process after it finished:")); 
 var timeToSeconds = time*60;
-print(pName+" will be Killed after "+time+" Minutes or "+timeToSeconds+" Seconds!");
-setTimeout(function () {
-  run("taskkill /IM "+pName+" /F"); //do something once
+print(pName+"will be Killed after "+time+" Minutes or "+timeToSeconds+" Seconds!");
+setTimeout(function () {//do something once
+	var commands = "taskkill /IM "+pName+" /F";
+	run(commands); //Async
+	//run(commands, "sync"); //Sync
 }, parseInt(time*1000*60));
-
