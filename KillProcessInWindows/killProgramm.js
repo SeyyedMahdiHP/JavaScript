c51efd41a07c35087b56adb8d7f7ac4475cea13e
SeyyedMahdi <seyyedmahdihp@gmail.com>
2017-12-07 01:00:44 +0330

    Update killProgramm.js

--- KillProcessInWindows/killProgramm.js
+++ KillProcessInWindows/killProgramm.js
@@ -3,6 +3,7 @@
 // smahdi1991@gmail.com
 // webhoma.ir
 // this is a console app, that by using it, you can set a time in Minutes to kill a programm after the time has been passed.
+//its the best way for gamers that do not want to put too much time on a beloved Game, and have a set time for their games
 const execSync = require('child_process').execSync; //synchron cmd exec
 const execASync = require('node-cmd'); 				//Asynchron cmd exec
 
