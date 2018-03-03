0dd2830f67aa0fdde8b07924f9ab9e07a9642fbb
SeyyedMahdi <smahdi1991@gmail.com>
2018-03-03 11:48:17 +0330

    bootstrap navbar reuseable DesignPattern

---
+++ ResponsiveDesign/reuseableDesign_bootstrap_navBar/index.php
@@ -0,0 +1,7 @@
+<?php require_once ('snippets/HEADER.php');?>
+    <div class="container">
+        <div class="div row">
+            <h1>Hello, world!</h1>
+        </div>
+    </div>
+<?php  require_once('snippets/FOOTER.php');?>
