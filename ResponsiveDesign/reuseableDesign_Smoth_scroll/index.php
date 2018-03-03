b9efa8725fc0349a768d8982533bd9d6adec12c3
SeyyedMahdi <smahdi1991@gmail.com>
2018-03-03 16:27:42 +0330

    Smoth scroll using jquery animate

---
+++ ResponsiveDesign/reuseableDesign_Smoth_scroll/index.php
@@ -0,0 +1,13 @@
+<?php require_once ('snippets/HEADER.php');?>
+<div id="scrol">
+   <div class="jumbotron jumbotron-fluid" id="HomeSection">
+       <h1 id="jumbotronHeading" class="display-3 jumbotronTextOnImages text-center">jumbotron fully responsive image</h1>
+       <img id="jumbotronImage" src="images/Youtube_2Bnew_2Bheader_u0ruil.png" alt="jumbotron image">
+   </div>
+    <div class="jumbotron jumbotron-fluid" id="AboutSection">
+        <h1 id="jumbotronHeading" class="display-3 jumbotronTextOnImages text-center">jumbotron fully responsive image</h1>
+        <img id="jumbotronImage" src="images/Youtube_2Bnew_2Bheader_u0ruil.png" alt="jumbotron image">
+    </div>
+</div>
+
+<?php  require_once('snippets/FOOTER.php');?>
