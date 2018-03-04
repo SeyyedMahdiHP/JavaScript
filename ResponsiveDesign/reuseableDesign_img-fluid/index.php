79819b3848a078ad7ae940f336f3bbdb298368e5
SeyyedMahdi <smahdi1991@gmail.com>
2018-03-04 10:08:08 +0330

    img-fluid team section

---
+++ ResponsiveDesign/reuseableDesign_img-fluid/index.php
@@ -0,0 +1,65 @@
+<?php require_once ('snippets/HEADER.php');?>
+   <div class="jumbotron jumbotron-fluid" id="HomeSection">
+       <h1 id="jumbotronHeading" class="display-3 jumbotronTextOnImages text-center">Heading</h1>
+       <img id="jumbotronImage" src="images/Youtube_2Bnew_2Bheader_u0ruil.png" alt="jumbotron image">
+   </div>
+    <div class="container-fluid" id="TeamSection">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Our Awesome Team</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consequatur exercitationem harum nulla quis voluptates! Animi atque debitis doloremque eius molestias mollitia, reprehenderit! Facere nihil odit rem reprehenderit voluptatibus voluptatum?</p>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-lg-3 col-sm-6 col-xs-12 teamMemberContainer"><img
+                        src="images/Team/thumbnails/thumbnails.png" alt=""
+                        class="img-fluid img-thumbnail teamMemberImage">
+                <h4>John</h4>
+                <p class="text-muted">Developer</p>
+                <p>John is the most Awesome Person in the world!</p>
+                <div class="col-12">
+                    <a href="" target="_blank"><img alt="Twitter" class="socialImages" src="https://simplesharebuttons.com/images/somacro/twitter.png"></a>
+                    <a href="" target="_blank"><img alt="Facebook" class="socialImages" src="https://simplesharebuttons.com/images/somacro/facebook.png"></a>
+                    <a href="" target="_blank"><img alt="Google" class="socialImages" src="https://simplesharebuttons.com/images/somacro/google.png"></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-xs-12 teamMemberContainer"><img
+                        src="images/Team/thumbnails/thumbnails.png" alt=""
+                        class="img-fluid img-thumbnail teamMemberImage">
+                <h4>John</h4>
+                <p class="text-muted">Developer</p>
+                <p>John is the most Awesome Person in the world!</p>
+                <div class="col-12">
+                    <a href="" target="_blank"><img alt="Twitter" class="socialImages" src="https://simplesharebuttons.com/images/somacro/twitter.png"></a>
+                    <a href="" target="_blank"><img alt="Facebook" class="socialImages" src="https://simplesharebuttons.com/images/somacro/facebook.png"></a>
+                    <a href="" target="_blank"><img alt="Google" class="socialImages" src="https://simplesharebuttons.com/images/somacro/google.png"></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-xs-12 teamMemberContainer"><img
+                        src="images/Team/thumbnails/thumbnails.png" alt=""
+                        class="img-fluid img-thumbnail teamMemberImage">
+                <h4>John</h4>
+                <p class="text-muted">Developer</p>
+                <p>John is the most Awesome Person in the world!</p>
+                <div class="col-12">
+                    <a href="" target="_blank"><img alt="Twitter" class="socialImages" src="https://simplesharebuttons.com/images/somacro/twitter.png"></a>
+                    <a href="" target="_blank"><img alt="Facebook" class="socialImages" src="https://simplesharebuttons.com/images/somacro/facebook.png"></a>
+                    <a href="" target="_blank"><img alt="Google" class="socialImages" src="https://simplesharebuttons.com/images/somacro/google.png"></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-xs-12 teamMemberContainer"><img
+                        src="images/Team/thumbnails/thumbnails.png" alt=""
+                        class="img-fluid img-thumbnail teamMemberImage">
+                <h4>John</h4>
+                <p class="text-muted">Developer</p>
+                <p>John is the most Awesome Person in the world!</p>
+                <div class="col-12">
+                    <a href="" target="_blank"><img alt="Twitter" class="socialImages" src="https://simplesharebuttons.com/images/somacro/twitter.png"></a>
+                    <a href="" target="_blank"><img alt="Facebook" class="socialImages" src="https://simplesharebuttons.com/images/somacro/facebook.png"></a>
+                    <a href="" target="_blank"><img alt="Google" class="socialImages" src="https://simplesharebuttons.com/images/somacro/google.png"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php  require_once('snippets/FOOTER.php');?>
