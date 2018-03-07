8954ae53409f852983899a37bd136d052aefe582
SeyyedMahdi <smahdi1991@gmail.com>
2018-03-07 10:08:18 +0330

    bootstrap blogpost card class

---
+++ ResponsiveDesign/reuseableDesign_bootstrap-card-blogpost/index.php
@@ -0,0 +1,155 @@
+<?php require_once ('snippets/HEADER.php');?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!--Carousel Container-->
+                <div id="AwesomeCarousel" class="carousel slide" data-ride="carousel">
+                    <!--Carousel Indicators-->
+                    <ol class="carousel-indicators">
+                        <li data-target="#AwesomeCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#AwesomeCarousel" data-slide-to="1"></li>
+                        <li data-target="#AwesomeCarousel" data-slide-to="2"></li>
+                    </ol>
+                    <!--Carousel Items's Container-->
+                    <div class="carousel-inner" role="listbox">
+                        <!--Carousel Items-->
+                        <div class="carousel-item active">
+                            <img class="d-block img-fluid carouselImg" src="images/slider1.jpg"  alt="">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3>Mountains</h3>
+                                <p>mountains are the earth pillars.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid carouselImg" src="images/Youtube_2Bnew_2Bheader_u0ruil.png"  alt="">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3>Creative</h3>
+                                <p>Creative Design is our JOb.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid carouselImg" src="images/slider2.jpg"  alt="">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3>Mr. lion</h3>
+                                <p>lion king is a good king:-D</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Forward Backward Carousel Buttons-->
+                    <a href="#AwesomeCarousel" class="carousel-control-prev" data-slide="prev" role="button">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    </a>
+                    <a href="#AwesomeCarousel" class="carousel-control-next" data-slide="next" role="button">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post1.jpg" alt="The invisible revolution" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">The invisible revolution</h4>
+                        </a>
+                        <p class="card-text">Looking for the next wave of breakthrough technology? It’s all around you.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post2.jpg" alt="Inside Building 87" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">Inside Building 87</h4>
+                        </a>
+                        <p class="card-text">Explore three futuristic Microsoft hardware labs.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post1.jpg" alt="The invisible revolution" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">The invisible revolution</h4>
+                        </a>
+                        <p class="card-text">Looking for the next wave of breakthrough technology? It’s all around you.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post2.jpg" alt="Inside Building 87" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">Inside Building 87</h4>
+                        </a>
+                        <p class="card-text">Explore three futuristic Microsoft hardware labs.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post1.jpg" alt="The invisible revolution" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">The invisible revolution</h4>
+                        </a>
+                        <p class="card-text">Looking for the next wave of breakthrough technology? It’s all around you.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post2.jpg" alt="Inside Building 87" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">Inside Building 87</h4>
+                        </a>
+                        <p class="card-text">Explore three futuristic Microsoft hardware labs.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post1.jpg" alt="The invisible revolution" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">The invisible revolution</h4>
+                        </a>
+                        <p class="card-text">Looking for the next wave of breakthrough technology? It’s all around you.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 cardContainer">
+                <div class="card">
+                    <a href="#">
+                        <img src="images/post2.jpg" alt="Inside Building 87" class="card-img-top cardImage">
+                    </a>
+                    <div class="card-block">
+                        <a href="#">
+                            <h4 class="card-title">Inside Building 87</h4>
+                        </a>
+                        <p class="card-text">Explore three futuristic Microsoft hardware labs.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php  require_once('snippets/FOOTER.php');?>
