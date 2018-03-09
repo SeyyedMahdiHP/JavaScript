d27459e2772d1ce820c2dfc6e54317ad40c5ea60
SeyyedMahdi <smahdi1991@gmail.com>
2018-03-09 20:14:05 +0330

    blog post image

---
+++ ResponsiveDesign/reuseableDesign_blogPostsPage_withSidebar/post.php
@@ -0,0 +1,37 @@
+<?php require_once ('snippets/HEADER.php');?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-9">
+            <h1> The Halo 5</h1>
+            <img id="PostImagebanner" src="images/halo-5-effect-marquee-tablet.jpg" alt="">
+        <h3 id="PostSnapShot">Jeremie Sloan’s garage in Oregon City teems with life-size “Halo” helmets, chest plates and codpieces. All are built with the goal of perfect fidelity to the “Halo” series of video games.
+            Elsewhere in his labyrinth of basement workspaces, Sloan uses pepakura, a Japanese papercraft technique, to match and glue thousands of tiny serial-numbered paper tabs into the models for his costumes. It takes weeks to construct a single Master Chief helmet out of pepakura — and that’s before the sanding and other tedious steps can even begin.</h3>
+        <h6>Xbox’s signature franchise is more than the sum of its parts</h6>
+        <p>“Some people assume that ‘Halo’ cosplayers are a bunch of teenagers with nothing better to do,” said Sloan, a 6’ 4” crane mechanic, Marine and soon-to-be grandfather. “But ‘Halo’ got me through months in a wheelchair after I broke my pelvis in a construction accident. I love the game. And I want to keep giving back to the ‘Halo’ universe that has been such an important part of my life.”
+            Sloan is not alone in his dedication to all things “Halo.” He is a member of the 405th Infantry Division, a global network of “Halo” cosplayers, prop builders and superfans. The wider fan base, called The Halo Nation, numbers in the millions. Since the launch of the first “Halo” game almost 15 years ago, they have elevated it into one of the world’s largest entertainment franchises, with more than 65 million copies sold and 6 billion hours of gameplay logged.</p>
+        <div class="text-center">
+            <img class="postArticleImage" src="images/jeremy-sloan-halo-5-pepakura-helmet.jpg" alt="">
+        </div>
+            <h4 id="PostLinkHeader">More Useful Links</h4>
+            <ul>
+                <li><a href="https://news.microsoft.com/stories/computingcancer/">Computing Cancer</a></li>
+                <li><a href="https://news.microsoft.com/stories/building87/">Inside Building 87</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-3">
+            <h2>Popular</h2>
+            <hr class="postSidebarLine">
+            <div class="row">
+                <div class="col-lg-12 col-md-6 postPopularContainer">
+                    <img class="postPopularImage" src="images/halo-5-character-john.png" alt="">
+                    <span> Master Chief</span>
+                </div>
+                <div class="col-lg-12 col-md-6 postPopularContainer">
+                    <img class="postPopularImage" src="images/halo-5-character-fred.png" alt="">
+                    <span>  Lieutenant, Junior Grade</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require_once ('snippets/FOOTER.php');?>
---
+++ ResponsiveDesign/reuseableDesign_blogPostsPage_withSidebar/snippets/HEADER.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+<!--      integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">-->
+    <!--index.css-->
+      <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+      <!--navbar-brand-->
+      <a href="index.php" class="navbar-brand">
+          <img src="images/logo.png" alt="logo">
+      </a>
+      <!--navbar button toggle -->
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#collapsableContent">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <!--collapsible content-->
+      <div class="navbar-collapse collapse" id="collapsableContent" aria-expanded="false" style="">
+          <!--navbar links-->
+          <ul class="navbar-nav mr-auto">
+
+          </ul>
+          <!--form search-->
+          <form class="form-inline my-2 my-lg-0" action="/action_page.php">
+              <input class="form-control mr-sm-2" type="text" placeholder="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+      </div>
+
+  </nav>
