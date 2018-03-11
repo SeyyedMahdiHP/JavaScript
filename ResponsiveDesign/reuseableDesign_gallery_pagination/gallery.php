13e496b9b433beac5eb12be61fa816b532c430a4
SeyyedMahdi <smahdi1991@gmail.com>
2018-03-11 09:43:02 +0330

    galery + pagination

---
+++ ResponsiveDesign/reuseableDesign_gallery_pagination/gallery.php
@@ -0,0 +1,78 @@
+<?php  require_once('snippets/HEADER.php');?>
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>Your Lite Photo Gallery</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/Youtube_2Bnew_2Bheader_u0ruil.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6 col-12 thumbnailContainer">
+            <img class="img-thumbnail" src="images/halo-5-character-fred.png" alt="">
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-12 text-center">
+            <nav>
+                <ul id="PaginationList" class="pagination">
+                    <li class="page-item"><a href="#" class="page-link">Previous</a></li>
+                    <li class="page-item"><a href="#" class="page-link">1</a></li>
+                    <li class="page-item"><a href="#" class="page-link">2</a></li>
+                    <li class="page-item"><a href="#" class="page-link">3</a></li>
+                    <li class="page-item"><a href="#" class="page-link">4</a></li>
+                    <li class="page-item"><a href="#" class="page-link">Next</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<?php  require_once('snippets/FOOTER.php');?>
---
+++ ResponsiveDesign/reuseableDesign_gallery_pagination/profile.php
@@ -0,0 +1,13 @@
+<?php  require_once('snippets/HEADER.php');?>
+<div class="jumbotron jumbotron-fluid" id="ProfileJumbotron">
+    <div class="row">
+        <div class="col-12">
+            <img id="ProfileBannerImage" src="images/jeremy-sloan-halo-5-pepakura-helmet.jpg" alt="">
+            <div class="carousel-caption">
+                <h3>Halo5</h3>
+                <p>this one of the most beloved game</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php  require_once('snippets/FOOTER.php');?>
